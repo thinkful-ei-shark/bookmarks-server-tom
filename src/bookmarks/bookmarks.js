eb1ae292414687b7cb0ed2868c79346a8c1c18b8
nickjang <nickjang@alumni.upenn.edu>
2020-10-23 14:58:11 -0400

    Add /GET bookmarks/:id

--- src/bookmarks/bookmarks.js
+++ src/bookmarks/bookmarks.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const { v4: uuid } = require('uuid');
-const logger = require('../logging.js')
-const bookmarkRouter = express.Router()
-const bookmarks = require('../store.js')
+const logger = require('../logging.js');
+const bookmarkRouter = express.Router();
+const bookmarks = require('../store.js');
 
+bookmarkRouter.route('/bookmarks')
+    .get((req, res) => {
+        res.json(bookmarks);
+    });
 
+bookmarkRouter.route('/bookmarks/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        if (!id) {
+            logger.error('id not found');
+            return res.status(404).send('Invalid data');
+        }
 
-bookmarkRouter.route('/bookmarks').get((req, res) => {
-    res
-        .json(bookmarks)
-})
+        const index = bookmarks.findIndex(bookmark =>
+            String(bookmark.id) === String(id));
 
+        if (index === -1) {
+            logger.error(`Could not find id: ${id}`);
+            return res.status(404).send('Invalid data');
+        }
 
+        res.json(bookmarks[index]);
+    });
 
-module.exports = bookmarkRouter
+module.exports = bookmarkRouter;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 const bookmarks = [{
+    id: '1143a007-744b-4bf5-b710-ce74fca67fb5',
     url: 'http://scrimba.com',
     title: 'Scrimba',
     rating: 5,
     description: 'Coding tutorials'
 }]
 
-module.exports = { bookmarks }
+module.exports = bookmarks;
